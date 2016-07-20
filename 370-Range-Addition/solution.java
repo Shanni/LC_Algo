535862a38894ed614380e0848b184041c47d14b4
Shan Liu <shan.liu03@gmail.com>
2016-07-19 22:26:35 -0400

    [370. Range Addition][Accepted]committed by LeetCode Extension

--- 370-Range-Addition/solution.java
+++ 370-Range-Addition/solution.java
@@ -1,17 +1,14 @@
 public class Solution {
     public int[] getModifiedArray(int length, int[][] updates) {
-        int[] array=new int[length];
+        int[] result=new int[length];
         for(int[] update: updates){
-            array[update[0]]+=update[2];
+            result[update[0]]+=update[2];
             if(update[1]+1<length)
-                array[update[1]+1]-=update[2];
+                result[update[1]+1]-=update[2];
         }
-        int max=Integer.MIN_VALUE;
         for(int i=1;i<length;i++){
-            array[i]+=array[i-1];
-            if(array[i]>max)
-                max=array[i];
+            result[i]+=result[i-1];
         }
-        return array;
+        return result;
     }
 }
